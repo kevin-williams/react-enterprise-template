9f36d8645991f4c4163c70482b1179a0482ab596
Kevin Williams <kwilliams@discounttire.com>
2018-05-02 10:52:59 -0700

    Converted from sagas to thunk

--- src/containers/location/locationActions.js
+++ src/containers/location/locationActions.js
@@ -1,7 +1,45 @@
+import axios from 'axios';
 import * as c from './locationConstants';
+import { DEFAULT_SERVICE_STATUS } from '../../utils';
 
-// updateZip action will post the GET_ZIP action to redux
-export const updateZip = zipCode => ({
-  type: c.GET_ZIP,
-  zipCode,
-});
+export const updateZip = zipCode => dispatch => {
+  console.log('calling getZipCode with zipCode=', zipCode);
+  if (zipCode.length < 5) {
+    dispatch({
+      type: c.GET_ZIP_SUCCESS,
+      zipCode: zipCode,
+      city: '',
+      state: '',
+      latitude: 0,
+      longitude: 0,
+      status: DEFAULT_SERVICE_STATUS,
+    });
+
+    return;
+  }
+
+  axios
+    .get('/api/location/' + zipCode)
+    .then(resp => {
+      if (resp.data && !resp.data.status.serviceFailure) {
+        dispatch({
+          type: c.GET_ZIP_SUCCESS,
+          zipCode: zipCode,
+          city: resp.data.zipCode['place name'],
+          state: resp.data.zipCode.state,
+          latitude: resp.data.zipCode.latitude,
+          longitude: resp.data.zipCode.longitude,
+          status: resp.data.status,
+        });
+      } else {
+        dispatch({ type: c.GET_ZIP_FAILURE, zipCode: '', status: resp.data.status });
+      }
+    })
+    .catch(error => {
+      dispatch({
+        type: c.GET_ZIP_FAILURE,
+        zipCode: '',
+        status: parseServiceErrorStatus('Error looking up zip code', error),
+      });
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Location from 'containers/location/Location';
 import Header from 'containers/header/Header';
 import Homepage from 'containers/homepage/Homepage';
-import store from 'store/reduxStore';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import reducers from './reducers';
 
 /**
  * Set up the redux store for the app.
@@ -15,7 +16,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
  * Set up the main react-router routes for the app.   As of react-router v4, Routes can be nested throughout the app.
  */
 render(
-  <Provider store={store}>
+  <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
     <Router>
       <div>
         <Route component={Header} />
